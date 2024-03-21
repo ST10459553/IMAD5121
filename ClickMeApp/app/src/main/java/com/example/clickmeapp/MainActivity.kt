5fcaa407635f2af795ae971077c49ea83ea11cb0
Joel Kit <joelktg9@gmail.com>
2024-03-21 13:08:53 +0200

    iniitial commit

--- ClickMeApp/app/src/main/java/com/example/clickmeapp/MainActivity.kt
+++ ClickMeApp/app/src/main/java/com/example/clickmeapp/MainActivity.kt
@@ -4,13 +4,19 @@ import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Spinner
+import android.widget.Switch
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+enum class Languages {
+    English,
+    French,
+    Zulu
+}
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,16 +27,32 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
         val clickMeBtn=findViewById<Button>(R.id.clickMeBtn)
         var inputField=findViewById<EditText>(R.id.nameInput)
         var welcomeTv=findViewById<TextView>(R.id.WelcomeTv)
+        val switchLang=findViewById<Switch>(R.id.switchFr)
+//        var engGreeting="Welcome,${inputField.text}"
+//        var frGreeting="Bienvenue,${inputField.text}"
+
+       
 
 
         clickMeBtn.setOnClickListener {
             Toast.makeText(this@MainActivity, "Button Clicked",Toast.LENGTH_LONG).show()
-            welcomeTv.text="Welcome , ${inputField.text}"
-//            clearing the field after clicking
+//            welcomeTv.text="Welcome , ${inputField.text}"
+            var engGreeting="Welcome , ${inputField.text}"
+            var frGreeting="Bienvenue , ${inputField.text}"
+
+            if (switchLang.isChecked){
+                welcomeTv.text=frGreeting
+            }
+            else{
+                welcomeTv.text=engGreeting
+            }
+
             inputField.text.clear()
+//            clearing the field after clicking
 
         }
 
--- LanguageSwitcherApp/app/src/main/java/com/example/languageswitcherapp/MainActivity.kt
+++ LanguageSwitcherApp/app/src/main/java/com/example/languageswitcherapp/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.languageswitcherapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +21,36 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val clickMeBtn=findViewById<Button>(R.id.clickMeBtn)
+        var inputField=findViewById<EditText>(R.id.nameInput)
+        var welcomeTv=findViewById<TextView>(R.id.WelcomeTv)
+        val switchLang=findViewById<Switch>(R.id.swtichFr)
+        var engGreeting="Welcome,${inputField.text}"
+        var frGreeting="Bievenue , ${inputField.text}"
+
+
+
+
+
+
+
+
+
+        clickMeBtn.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Button Clicked", Toast.LENGTH_LONG).show()
+            welcomeTv.text="Welcome , ${inputField.text}"
+//            clearing the field after clicking
+            if (switchLang.isChecked){
+                welcomeTv.text=frGreeting
+            }
+            else{
+                welcomeTv.text=engGreeting
+            }
+
+            inputField.text.clear()
+
+        }
+
+
     }
 }
